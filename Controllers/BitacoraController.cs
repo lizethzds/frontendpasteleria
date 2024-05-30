adf6244d5c497293202c446db7b3ba5db77cdc77
BrandonSS2505 <brandonaguilerahdez@gmail.com>
2024-05-29 23:54:47 -0600

    Base para el front
    es importante mencionar que no va a correr porque faltan modificaciones para que este acorde al api

---
+++ Controllers/BitacoraController.cs
@@ -0,0 +1,23 @@
+using frontendnet.Models;
+using frontendnet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frontendnet;
+
+public class BitacoraController(BitacoraClientService bitacora) : Controller
+{
+    public async Task<IActionResult> IndexAsync()
+    {
+        List<Bitacora>? lista = [];
+        try
+        {
+            lista = await bitacora.GetAsync();
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return RedirectToAction("Salir", "Auth");
+        }
+        return View(lista);
+    }
+}
